462d051fec9ccbcdc4f73d5e2f925ae18da12a59
Mirzarahmon <rahmonraxmatov@gmail.com>
2023-07-26 14:58:11 +0500

    added:js

--- script/app.js
+++ script/app.js
@@ -7,10 +7,10 @@ todolistForm.addEventListener("submit", createTask);
 
 const TASKLIST = [];
 
-function createTask(e){
+function createTask(e) {
   e.preventDefault();
-  if(todolistInput.value.trim().length > 0){
-    const date = new Date()
+  if (todolistInput.value.trim().length > 0) {
+    const date = new Date();
     const taskData = {
       taskName: todolistInput.value,
       taskDate: addZeroToTime(date.getDate()),
@@ -18,7 +18,7 @@ function createTask(e){
       taskMinute: addZeroToTime(date.getMinutes()),
       taskMonth: addZeroToTime(date.getMonth() + 1),
       taskYear: date.getFullYear(),
-      taskCompleted: false
+      taskCompleted: false,
     };
     TASKLIST.unshift(taskData);
     clearInput(todolistInput);
@@ -26,11 +26,8 @@ function createTask(e){
   }
 }
 
-function renderTasks(){
-  // 1 way
-  // todolistTaskContainer.innerHTML = "";
-  // 2 way
-  while(todolistTaskContainer.firstChild){
+function renderTasks() {
+  while (todolistTaskContainer.firstChild) {
     todolistTaskContainer.removeChild(todolistTaskContainer.firstChild);
   }
   TASKLIST.map((todo, index) => {
@@ -38,42 +35,45 @@ function renderTasks(){
     div.className = "todolist__item";
     div.setAttribute("data-order-number", index);
     div.innerHTML = `
-      <strong style="${todo.taskCompleted ? 'text-decoration: line-through' : 'text-decoration: none'}">${todo.taskName}</strong>
+      <strong style="${todo.taskCompleted ? "color:red" : "color:black;"}">${
+      todo.taskName
+    }</strong>
       <div class="todolist__actions">
         <button class="todolist-btn complete"> <i class="fas fa-check-circle"></i> Complete</button>
         <button class="todolist-btn edit"> <i class="fas fa-edit"></i>  Edit</button>
-        <button class="todolist-btn time"> <div class="additonal__time">${todo.taskDate}/${todo.taskMonth}/${todo.taskYear}</div> <i class="fas fa-clock"></i> ${todo.taskHour} : ${todo.taskMinute}</button>
+        <button class="todolist-btn time"> <div class="additonal__time">${
+          todo.taskDate
+        }/${todo.taskMonth}/${
+      todo.taskYear
+    }</div> <i class="fas fa-clock"></i> ${todo.taskHour} : ${
+      todo.taskMinute
+    }</button>
         <button class="todolist-btn delete"> <i class="fas fa-trash"></i> Delete</button>
       </div>
-    `
+    `;
     todolistTaskContainer.appendChild(div);
   });
 }
 
 todolistTaskContainer.addEventListener("click", (e) => {
   let taskIndex = +e.target.parentElement.parentElement.dataset.orderNumber;
-  if(e.target.classList.contains("complete")){
+  if (e.target.classList.contains("complete")) {
     TASKLIST[taskIndex].taskCompleted = !TASKLIST[taskIndex].taskCompleted;
     renderTasks();
-  }
-  else if(e.target.classList.contains("delete")){
+  } else if (e.target.classList.contains("delete")) {
     TASKLIST.splice(taskIndex, 1);
     renderTasks();
   }
-  else{
-    console.log("btn bosilmadi ")
-  }
-})
+});
 
 clearInputBtn.addEventListener("click", () => {
   clearInput(todolistInput);
-})
+});
 
-
-function clearInput(inp){
-  inp.value = ""
+function clearInput(inp) {
+  inp.value = "";
 }
 
-function addZeroToTime(time){
+function addZeroToTime(time) {
   return String(time).padStart(2, "0");
 }
